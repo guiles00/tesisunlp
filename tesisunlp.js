b6e72d8df1297a46370ae031fc72eaf19e743d89
guillermo <you@example.com>
2014-10-28 09:26:39 -0300

    todos los js

---
+++ tesisunlp.js
@@ -0,0 +1,50 @@
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/Attribute.js');
+document.head.appendChild(tesis_script);
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/Utils.js');
+document.head.appendChild(tesis_script);
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/listeners.js');
+document.head.appendChild(tesis_script);
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/tablednd.js');
+document.head.appendChild(tesis_script);
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/Recorder.js');
+document.head.appendChild(tesis_script);
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/localStorageManager.js');
+document.head.appendChild(tesis_script);
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/Manager.js');
+document.head.appendChild(tesis_script);
+
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/RConsole.js');
+document.head.appendChild(tesis_script);
+
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/main.js');
+document.head.appendChild(tesis_script);
+
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/edit.js');
+document.head.appendChild(tesis_script);
+
+
+var tesis_script = document.createElement('script');
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/PrimitiveTasks.js');
+document.head.appendChild(tesis_script);
+
+
+

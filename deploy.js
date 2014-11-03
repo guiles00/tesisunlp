dda8477bef1133af961729f36415c85ac719fa3d
guillermo <you@example.com>
2014-11-03 10:54:50 -0300

    deploy.js

---
+++ deploy.js
@@ -0,0 +1,5 @@
+var tesis_script = document.createElement('script');
+
+tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/tesisunlp.js');
+
+document.head.appendChild(tesis_script);

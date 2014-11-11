fee3d6db36ad3140a978639219be5b96728761a7
guillermo <you@example.com>
2014-11-11 09:32:26 -0300

    modifique deploy.js

--- deploy.js
+++ deploy.js
@@ -3,3 +3,8 @@ var tesis_script = document.createElement('script');
 tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/tesisunlp.js');
 
 document.head.appendChild(tesis_script);
+var tesis_css = document.createElement('link');
+
+tesis_css.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/tesisunlp.css');
+
+document.head.appendChild(tesis_css);

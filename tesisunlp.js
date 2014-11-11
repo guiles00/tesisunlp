a4925df74b6a3698e5a877d1a9222563df6219ad
guillermo <you@example.com>
2014-11-11 10:54:39 -0300

    asd

--- tesisunlp.js
+++ tesisunlp.js
@@ -1,11 +1,11 @@
-/*
-var tesis_css = document.createElement('link');
 
-tesis_css.setAttribute('href','https://raw.githubusercontent.com/guiles/unlptesis/master/tesisunlp.css');
-tesis_css.setAttribute('rel','stylesheet');
-tesis_css.setAttribute('type','text/css');
+var tesis1_css = document.createElement('link');
 
-document.head.appendChild(tesis_css);*/
+tesis1_css.setAttribute('href','https://raw.githubusercontent.com/guiles/unlptesis/master/tesisunlp.css');
+tesis1_css.setAttribute('rel','stylesheet');
+tesis1_css.setAttribute('type','text/css');
+
+document.head.appendChild(tesis1_css);
 //Este script es para prueba solamente, usar uglify para unir todos los archivos
 var tesis_script = document.createElement('script');
 tesis_script.setAttribute('src','https://raw.githubusercontent.com/guiles/unlptesis/master/Attribute.js');

8fec235521376fa09849b87cba1102fca2d58a72
Guiles <guiles00@yahoo.com>
2015-09-14 11:44:49 -0300

    agregue atributo group a abstractTask

--- js/AbstractTask.js
+++ js/AbstractTask.js
@@ -11,7 +11,9 @@ this.msg = "AbstractTask"
 this.type = "AbstractTask"
 this.precondition = {};
 this.taskTitle = taskTitle;
-this.group = 0//Object.create(StateAttribute).init({'value':0,'htmlId':'group_id','label':'group'}) ;
+//this.group = 0//Object.create(StateAttribute).init({'value':0,'htmlId':'group_id','label':'group'}) ;
+this.group = Object.create(StateAttribute).init({'value':0,'htmlId':'group_id','label':'group'}) ;
+
 
 }
 

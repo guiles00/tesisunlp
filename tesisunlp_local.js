32f4bfb484ea4c83c4700fcdcefb3e2ca2c7fc26
guillermo <you@example.com>
2016-03-09 21:11:25 -0300

    no modifique nada al final

--- tesisunlp_local.js
+++ tesisunlp_local.js
@@ -7,21 +7,17 @@
 **  css --> CSS 
 **  exports --> donde se guardan los procedures en .json
 **/
-
-var absolute_path = 'http://localhost/content/tesisunlp/';
-
 var tesis_css = document.createElement('link');
 tesis_css.setAttribute("rel","stylesheet");
 tesis_css.setAttribute("type","text/css");
-//tesis_css.setAttribute("href","http://127.0.0.1/content/tesisunlp/css/tesisunlp.css");
-tesis_css.setAttribute("href",absolute_path+"/css/tesisunlp.css");
+tesis_css.setAttribute("href","http://127.0.0.1/tesisunlp/css/tesisunlp.css");
 document.head.appendChild(tesis_css);
-
+//alert(' la concha de tu madre');
 //Load scripts after loading
 window.onload = (function(){
 
-	
-	//var absolute_path = 'var/www/content/tesisunlp/js/';
+	var absolute_path = 'http://127.0.0.1/tesisunlp/';
+	//var absolute_path = 'var/www/tesis/tesisunlp/js/';
 
 	function loadScript (dir, file) {
 	 var scr = document.createElement("script");
@@ -36,8 +32,8 @@ window.onload = (function(){
 	        loadScript(absolute_path+'./core/',objects[i]+'.js');
 	};
 
-	var tasks = ['PrimitiveTasks','AbstractTask','AugmentedTask','ComposedTask','IfTask','Attribute','Utils'
-	,'NotasTask','SumatoriaTask','ConcatStringTask','HighLightTask'
+	var tasks = ['PrimitiveTasks','AbstractTask','AugmentedTask','Attribute','ComposedTask','Attribute','Utils'
+	,'NotasTask','SumatoriaTask','ConcatStringTask','IfTask','HighLightTask'
 	,'FillInputTask','TextAreaTask','SelectOptionTask','CheckBoxTask'
 	,'ClickLinkTask','ClickInputTask','RadioTask','DataCollectionTask','UrlTask'];
 	//Load tasks
@@ -58,54 +54,54 @@ window.onload = (function(){
 
 /*
 var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/libs/tablednd.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/libs/tablednd.js');
 document.head.appendChild(tesis_script);
 */
 /*var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/core/listeners.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/core/listeners.js');
 document.head.appendChild(tesis_script);
 
 
 var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/core/localStorageManager.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/core/localStorageManager.js');
 document.head.appendChild(tesis_script);
 
 var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/core/StorageManager.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/core/StorageManager.js');
 document.head.appendChild(tesis_script);
 
 var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/core/main.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/core/main.js');
 document.head.appendChild(tesis_script);
 
 
 var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/core/edit.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/core/edit.js');
 document.head.appendChild(tesis_script);
 
 *///Este script es para prueba solamente, usar uglify para unir todos los archivos
 /*var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/js/Attribute.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/js/Attribute.js');
 document.head.appendChild(tesis_script);
 
 var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/js/Utils.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/js/Utils.js');
 document.head.appendChild(tesis_script);
 */
 
 
 /*var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/Recorder.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/Recorder.js');
 document.head.appendChild(tesis_script);
 */
 
 /*var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/Manager.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/Manager.js');
 document.head.appendChild(tesis_script);
 */
 
 /*var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/RConsole.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/RConsole.js');
 document.head.appendChild(tesis_script);
 */
 
@@ -113,7 +109,7 @@ document.head.appendChild(tesis_script);
 
 /*
 var tesis_script = document.createElement('script');
-tesis_script.setAttribute('src','http://127.0.0.1/content/tesisunlp/js/PrimitiveTasks.js');
+tesis_script.setAttribute('src','http://127.0.0.1/tesis/tesisunlp/js/PrimitiveTasks.js');
 document.head.appendChild(tesis_script);*/
 
 
